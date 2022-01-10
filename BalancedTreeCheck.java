438646dbc786c62d5f8aa6bed6a0f705dae7ba81
dmanjunathakinik <deepakmkini@gmail.com>
2022-01-10 15:33:18 -0800

    Create BalancedTreeCheck.java

---
+++ BalancedTreeCheck.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    //https://leetcode.com/problems/balanced-binary-tree/submissions/
+    public boolean isBalanced(TreeNode root) {
+        
+        if(root == null){
+            return true;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        
+        if(Math.abs(leftHeight-rightHeight)>1){
+            return false;
+        }else{
+            return isBalanced(root.left) && isBalanced(root.right);
+        }
+        
+    }
+    
+    public int height(TreeNode root)
+    {
+        if(root == null)
+            return 0;
+        return Math.max(height(root.left) , height(root.right)) + 1;
+    }
+}
