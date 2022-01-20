b065c45419ecdbd62d3db8397467efab4385a783
dmanjunathakinik <deepakmkini@gmail.com>
2022-01-19 18:14:01 -0800

    Create InvertBinaryTree.java

---
+++ InvertBinaryTree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class InvertBinaryTree {
+    //https://leetcode.com/problems/invert-binary-tree/submissions/
+    public TreeNode invertTree(TreeNode root) {
+        if(root==null){
+        return root;
+    }
+ 
+    invertTree(root.left);
+    invertTree(root.right);
+ 
+    TreeNode t = root.left;
+    root.left = root.right;
+    root.right = t;
+ 
+    return root;
+    }
+    
+     
+}
