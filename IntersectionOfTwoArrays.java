42fc9235d70b1dbf250d42a7bedfabad96b1ad28
dmanjunathakinik <deepakmkini@gmail.com>
2022-02-17 23:09:22 -0800

    Create IntersectionOfTwoArrays.java

---
+++ IntersectionOfTwoArrays.java
@@ -0,0 +1,26 @@
+class IntersectionOfTwoArrays {
+    //https://leetcode.com/problems/intersection-of-two-arrays/submissions/
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        
+        int j=0;
+        int k=0;
+        HashSet<Integer> list = new HashSet<>();
+        
+        while(j<nums1.length && k<nums2.length){
+            if(nums1[j] == nums2[k]){
+                list.add(nums1[j]);
+                j++;
+                k++;
+            }else if(nums1[j] < nums2[k]){
+                j++;
+            }else if(nums2[k] < nums1[j]){
+                k++;
+            }
+        }
+        int[] arr = list.stream().mapToInt(m -> m).toArray();
+        return arr;
+    }
+}
