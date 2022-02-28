c4d7c0c07c046d7cf9684094b5d34d23b0fda89b
dmanjunathakinik <deepakmkini@gmail.com>
2022-02-28 14:20:44 -0800

    Update WordValidAfterSubstitutions.java

--- WordValidAfterSubstitutions.java
+++ WordValidAfterSubstitutions.java
@@ -13,4 +13,27 @@ class WordValidAfterSubstitutions {
             s = t;
         }
     }
+    
+    //Another solution
+    public boolean isValid2(String s) {
+        Stack<Character> st = new Stack<>();
+        
+        for(int i = 0 ; i < s.length() ; i++){
+            char ch = s.charAt(i);
+            
+            if(ch == 'a' || ch == 'b'){
+                st.push(ch);
+            }
+            else{
+                if(st.size() >= 2 && st.pop() == 'b' && st.pop() == 'a'){
+                    // no issue
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        
+        return st.size() == 0;
+    }
 }
