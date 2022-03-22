ffbb8d62e13258e9142db49ff8043ceb5cccd39d
dmanjunathakinik <deepakmkini@gmail.com>
2022-03-21 23:49:17 -0700

    Update FurthestBuilding.java

--- Heap/FurthestBuilding.java
+++ Heap/FurthestBuilding.java
@@ -1,29 +1,25 @@
 
 class FurthestBuilding {
     //https://leetcode.com/problems/furthest-building-you-can-reach/submissions/
+
     public int furthestBuilding(int[] heights, int bricks, int ladders) {
         // Create a priority queue with a comparator that makes it behave as a min-heap.
-        Queue<Integer> ladderAllocations = new PriorityQueue<>((a, b) -> a - b);
-        for (int i = 0; i < heights.length - 1; i++) {
-            int climb = heights[i + 1] - heights[i];
-            // If this is actually a "jump down", skip it.
-            if (climb <= 0) {
+        Queue<Integer> heap = new PriorityQueue<>((a, b) -> a - b);
+        for(int i=0;i<heights.length-1;i++){
+            int diff = heights[i + 1] - heights[i];
+            if(diff <0){
                 continue;
             }
-            // Otherwise, allocate a ladder for this climb.
-            ladderAllocations.add(climb);
-            // If we haven't gone over the number of ladders, nothing else to do.
-            if (ladderAllocations.size() <= ladders) {
+            heap.add(diff);
+            if(heap.size()<=ladders){
                 continue;
             }
-            // Otherwise, we will need to take a climb out of ladder_allocations
-            bricks -= ladderAllocations.remove();
-            // If this caused bricks to go negative, we can't get to i + 1
-            if (bricks < 0) {
+            bricks -= heap.remove();
+            if(bricks<0){
                 return i;
             }
         }
-        // If we got to here, this means we had enough materials to cover every climb.
+        
         return heights.length - 1;
     }
 }
