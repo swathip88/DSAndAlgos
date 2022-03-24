61750f0a5b0416a855f133454ba9951f539115b8
dmanjunathakinik <deepakmkini@gmail.com>
2022-03-24 12:50:27 -0700

    Update and rename MoveZeroes.java to Arrays/MoveZeroes.java

--- Arrays/MoveZeroes.java
+++ Arrays/MoveZeroes.java
@@ -1,12 +1,6 @@
 class MoveZeroes {
     //https://leetcode.com/problems/move-zeroes/submissions/
     public void moveZeroes(int[] nums) {
-        int count = 0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == 0){
-                count++;
-            }
-        }
         
         int i=0;
         int j=0;
