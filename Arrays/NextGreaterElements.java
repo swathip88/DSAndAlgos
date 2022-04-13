2daa862deeacd861092c81d9ff4699d86e23112d
dmanjunathakinik <deepakmkini@gmail.com>
2022-04-12 20:55:27 -0700

    Create NextGreaterElements.java

---
+++ Arrays/NextGreaterElements.java
@@ -0,0 +1,16 @@
+class NextGreaterElements {
+    //https://leetcode.com/problems/next-greater-element-ii/submissions/
+    public int[] nextGreaterElements(int[] nums) {
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = -1;
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[(i + j) % nums.length] > nums[i]) {
+                    res[i] = nums[(i + j) % nums.length];
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
